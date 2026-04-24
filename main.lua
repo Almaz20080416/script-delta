fbac41a05f9f5210486c056cc44892d6282d4aa1
Almaz20080416 <tuganbaialmaz@gmail.com>
2026-04-24 20:31:10 +0500

    Refactor script to use Rayfield library

--- main.lua
+++ main.lua
@@ -1,37 +1,59 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SteelyXeno/SolarisLib/main/source.lua"))()
-local Window = Library:New({Name = "AOT: Revolution Helper", FolderToSave = "AOTR_Settings"})
+local Window = Rayfield:CreateWindow({
+   Name = "AOT: Revolution Helper",
+   LoadingTitle = "Загрузка скрипта...",
+   LoadingSubtitle = "by Almaz",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "AOTR_Settings",
+      FileName = "MainConfig"
+   }
+})
 
-local Tab = Window:Tab("Автофарм")
-local Section = Tab:Section("Основные функции")
+local Tab = Window:CreateTab("Автофарм", 4483362458) -- Иконка фарм
 
 _G.AutoAttack = false
 
--- Функция авто-атаки
-Section:Toggle("Авто-удары (Kill Aura)", false, "KillAura", function(state)
-    _G.AutoAttack = state
-    if state then
-        spawn(function()
+Tab:CreateToggle({
+   Name = "Авто-удары (Kill Aura)",
+   CurrentValue = false,
+   Flag = "KillAura",
+   Callback = function(Value)
+      _G.AutoAttack = Value
+      if Value then
+         spawn(function()
             while _G.AutoAttack do
-                local VirtualUser = game:GetService("VirtualUser")
-                VirtualUser:CaptureController()
-                VirtualUser:ClickButton1(Vector2.new(851, 158))
-                task.wait(0.1)
+               local VirtualUser = game:GetService("VirtualUser")
+               VirtualUser:CaptureController()
+               VirtualUser:ClickButton1(Vector2.new(851, 158))
+               task.wait(0.1)
             end
-        end)
-    end
-end)
+         end)
+      end
+   end,
+})
 
--- Ускорение бега
-Section:Slider("Скорость бега", 16, 300, 100, 1, "Speed", function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
+Tab:CreateSlider({
+   Name = "Скорость бега/полета",
+   Range = {16, 300},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 100,
+   Flag = "SpeedSlider",
+   Callback = function(Value)
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+   end,
+})
 
--- Удаление тумана
-local MiscTab = Window:Tab("Разное")
-local MiscSection = MiscTab:Section("Настройки")
-MiscSection:Button("Удалить туман", function()
-    game:GetService("Lighting").FogEnd = 100000
-end)
+local MiscTab = Window:CreateTab("Разное", 4483345998)
 
-Library:Notification("Загрузка", "Скрипт успешно запущен!")
+MiscTab:CreateButton({
+   Name = "Удалить туман",
+   Callback = function()
+      game:GetService("Lighting").FogEnd = 100000
+      Rayfield:Notify({Title = "Успех", Content = "Туман удален!"})
+   end,
+})
+
+Rayfield:Notify({Title = "Готово!", Content = "Скрипт успешно запущен!"})
